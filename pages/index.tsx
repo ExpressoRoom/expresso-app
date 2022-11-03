284636367fbc98bc2822aef32b1705a94049bad3
amberlyM <amberm1205@gmail.com>
2022-11-03 09:41:43 -0700

    messing with home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,38 @@ const socket = io('localhost:8000');
 
 export default function Home() {
   return (
-    <div>
-      <h1>
-        Welcome to Next
-      </h1>
+    <div className="container mx-auto">
+      <div className="border-2 border-sky-500">
+        Icon bar
+      </div>
+      <div>
+        homepage container
+        <h1>
+          Hi *username*
+        </h1>
+        <div>
+          left column
+          <div>
+            Social Feed box
+          </div>
+          <div>
+            current Projects box
+          </div>
+        </div>
+        <div>
+          right column
+          <div>
+            Calendar box
+          </div>
+          <div>
+            technical question box
+          </div>
+          <div>
+            call to action box
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
