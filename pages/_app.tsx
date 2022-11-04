9dbfbd478f4037f0f3b862108a2695792e495eb1
Tony Kang <tony.andrew.kang@gmail.com>
2022-11-04 12:25:54 -0700

    Fixed Calendar, edited nextconfigjs, added CSS imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
-import Layout from '../components/Layout';
+import "@fullcalendar/common/main.css";
+import "@fullcalendar/daygrid/main.css";
+import "@fullcalendar/timegrid/main.css";
 
+import Layout from '../components/Layout';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
--- pages/calendar/index.tsx
+++ pages/calendar/index.tsx
@@ -1,14 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import CalendarComponent from '../../components/Calendar';
+import React from 'react';
+import FullCalendar from "@fullcalendar/react";
+// The import order DOES MATTER here. If you change it, you'll get an error!
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import CalendarComponent from '../../components/Calendar'
 
-export default function CalendarPage() {
+export default function Home() {
   return (
-    <div className="border-2 border-yellow-600 flex items-center justify-center text-3xl ml-1/6 h-screen">
-      <h1 className="text-center">
-        <CalendarComponent />
-      </h1>
+    <div>
+      <CalendarComponent />
     </div>
   )
 }
